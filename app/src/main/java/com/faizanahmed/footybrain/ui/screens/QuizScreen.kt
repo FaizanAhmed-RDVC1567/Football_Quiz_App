f99b3901cc1229f8f3726e85fb7d3630436e7dc1
Faizan Ahmed <chfaizan.510@outlook.com>
2026-07-20 15:57:59 +0400

    Added the Scaffold UI element to the screen, to help differentiate between the status bar and the quiz screen.

--- app/src/main/java/com/faizanahmed/footybrain/ui/screens/QuizScreen.kt
+++ app/src/main/java/com/faizanahmed/footybrain/ui/screens/QuizScreen.kt
@@ -11,13 +11,31 @@ import com.faizanahmed.footybrain.viewmodel.QuizViewModel
 fun QuizScreen(category: String, viewModel: QuizViewModel, onFinished: (Int) -> Unit) {
     val state by viewModel.uiState.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Text("Quiz for $category", style = MaterialTheme.typography.headlineSmall)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("Score: ${state.score}")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { onFinished(state.score) }) {
+    /*
+    * The Scaffold bit here, is to put some appropriate space between the status bar at
+    * the top, and the top part of the app so that the two screens don't mix together,
+    * and the user experience becomes better.
+    */
+
+    Scaffold(
+        topBar = { AppTopBar("Choose an answer") },
+        contentWindowInsets = WindowInsets.safeDrawing // respects the status + nav bars
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Quiz for $category", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Score: ${state.score}")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { onFinished(state.score) }) {
             Text("Finish Quiz")
+            }
         }
     }
 }
