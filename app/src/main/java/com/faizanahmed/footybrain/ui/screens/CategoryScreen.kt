e920243d219bc54f2fe03d81415effb7171061ec
Faizan Ahmed <chfaizan.510@outlook.com>
2026-07-20 15:57:03 +0400

    Added the Scaffold UI element to the screen, to help differentiate between the status bar and the category screen.

--- app/src/main/java/com/faizanahmed/footybrain/ui/screens/CategoryScreen.kt
+++ app/src/main/java/com/faizanahmed/footybrain/ui/screens/CategoryScreen.kt
@@ -10,21 +10,36 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun CategoryScreen(onCategorySelected: (String) -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = { onCategorySelected("players") }, modifier = Modifier.fillMaxWidth()) {
-            Text("Players")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = { onCategorySelected("clubs") }, modifier = Modifier.fillMaxWidth()) {
-            Text("Clubs")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = { onCategorySelected("stadiums") }, modifier = Modifier.fillMaxWidth()) {
-            Text("Stadiums")
+
+    /*
+    * The Scaffold bit here, is to put some appropriate space between the status bar at
+    * the top, and the top part of the app so that the two screens don't mix together,
+    * and the user experience becomes better.
+    */
+
+    Scaffold(
+        topBar = { AppTopBar("Choose a category) },
+        contentWindowInsets = WindowInsets.safeDrawing // respects the status + nav bars
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = { onCategorySelected("players") }, modifier = Modifier.fillMaxWidth()) {
+                Text("Players")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { onCategorySelected("clubs") }, modifier = Modifier.fillMaxWidth()) {
+                Text("Clubs")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { onCategorySelected("stadiums") }, modifier = Modifier.fillMaxWidth()) {
+                Text("Stadiums")
+            }
         }
     }
 }
