58ff9a514c5bb7143f19af64f855077dbb23fafb
Faizan Ahmed <chfaizan.510@outlook.com>
2026-07-20 15:59:11 +0400

    Added the Scaffold UI element to the screen, to help differentiate between the status bar and the results screen.

--- app/src/main/java/com/faizanahmed/footybrain/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/faizanahmed/footybrain/ui/screens/ResultScreen.kt
@@ -10,15 +10,30 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ResultScreen(score: Int, onPlayAgain: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Final Score: $score", style = MaterialTheme.typography.headlineMedium)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onPlayAgain) {
-            Text("Play Again")
+
+    /*
+    * The Scaffold bit here, is to put some appropriate space between the status bar at
+    * the top, and the top part of the app so that the two screens don't mix together,
+    * and the user experience becomes better.
+    */
+
+    Scaffold(
+        topBar = { AppTopBar("View your results") },
+        contentWindowInsets = WindowInsets.safeDrawing // respects the status + nav bars
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Final Score: $score", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onPlayAgain) {
+                Text("Play Again")
+            }
         }
     }
 }
