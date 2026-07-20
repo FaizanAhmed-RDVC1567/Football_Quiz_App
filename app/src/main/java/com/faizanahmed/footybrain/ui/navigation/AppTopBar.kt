9d049a54d9a1c786d111bc869b779d6e4b34f930
Faizan Ahmed <chfaizan.510@outlook.com>
2026-07-20 15:55:41 +0400

    Created the top bar for the application, to improve the user's viewing experience.

---
+++ app/src/main/java/com/faizanahmed/footybrain/ui/navigation/AppTopBar.kt
@@ -0,0 +1,17 @@
+package com.faizanahmed.footybrain.ui.navigation
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(title: String) {
+    TopAppBar(
+        title = { Text(title) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
+}
